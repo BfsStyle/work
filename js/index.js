3e4f9e23720c3a695b59d276e524045a3dfaeea4
BfsStyle <Bbfs_style@163.com>
2017-10-25 21:25:41 +0800

    组长最美

---
+++ js/index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by zhousg on 2017/10/12.
+ */
+window.onload=function(){
+    /*1，顶部搜索*/
+    search();
+    /*2，轮播图*/
+    banner();
+    /*3，倒计时*/
+    downtime();
+};
+
+var search=function(){
+  /**/
+  /**/
+  /**/
+  var searchBox=document.querySelector('')
+};
+var banner=function(){
+    var banner=document.querySelector('.jd_')
+};
